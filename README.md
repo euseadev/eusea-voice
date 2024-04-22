3c51f324cecc23611c53bb8a75955260fa052576
euseaa <sweetyvac@gmail.com>
2024-04-22 06:11:47 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Eusea VOICE
+
+Çiçero(discord.gg/cicero) ekibi için yapmış olduğum botları sese sokma uygulaması.
+
+
+tokens.txt dosyasına tokenleri gir 
+config.json a ses kanal id sini gir
+
+run.bat ı aç
+
+ve mucizeyi gör
+
+Bilgisayarına [nodejs kur](https://nodejs.org)mayı unutma aptal orospu evladı
---
+++ index.js
@@ -0,0 +1,35 @@
+const { Client } = require("discord.js");
+const fs = require("fs");
+const config = require('./config.json')
+
+const tokenFile = "tokens.txt";
+
+const tokens = fs.readFileSync(tokenFile, "utf-8").split("\n").map((token) => token.trim());
+let i = 0;
+
+tokens.forEach((token, index) => {
+    const client = new Client();
+
+    client.login(token);
+    
+    client.on("ready", async () => {
+        setTimeout(async () => {
+            i += 1;
+            console.log(`[${i}] ${client.user.tag} olarak giriş yapıldı!`);
+            client.user.setStatus("dnd");
+            client.user.setActivity("euseadev 💗 çiçero");
+
+            const channelId = config.channelid;
+            const voiceChannel = client.channels.cache.get(channelId);
+
+            setTimeout(() => {
+                voiceChannel.join();
+            }, 250);
+
+        }, 500);
+    });
+});
+
+process.on("uncaughtException", (e) => {
+    return console.log(e.message)
+});
